d43945e62f881911a8ce2cf5b2a1e199454f7797
Adam Smith <hagansmith@gmail.com>
2018-04-02 12:52:52 -0500

    updates

--- javascripts/app.js
+++ javascripts/app.js
@@ -3,18 +3,3 @@
 "use strict";
 
 const app = angular.module("AdamSmith", ["ngAnimate", "ngRoute", "ngSanitize", "ui.bootstrap"]);
-
-app.directive("scroll", function ($window) {
-    return function(scope, element, attrs) {
-        angular.element($window).bind("scroll", function() {
-             if (this.pageYOffset >= 175) {
-               $('.navigation').addClass('navbar-fixed-top');
-               $('body').css('paddingTop', '75px');
-             } else {
-               $('.navigation').removeClass('navbar-fixed-top');
-               $('body').css('paddingTop', '0px');
-             }
-            scope.$apply();
-        });
-    };
-});
--- javascripts/controllers/NavCtrl.js
+++ javascripts/controllers/NavCtrl.js
@@ -8,3 +8,18 @@ app.controller("NavCtrl", function($scope, $window, DataService){
      $(this).parent().addClass("active");
   });
 });
+
+app.directive("scroll", function ($window) {
+  return function(scope, element, attrs) {
+      angular.element($window).bind("scroll", function() {
+           if (this.pageYOffset >= 175) {
+             $('.navigation').addClass('navbar-fixed-top');
+             $('body').css('paddingTop', '75px');
+           } else {
+             $('.navigation').removeClass('navbar-fixed-top');
+             $('body').css('paddingTop', '0px');
+           }
+          scope.$apply();
+      });
+  };
+});
